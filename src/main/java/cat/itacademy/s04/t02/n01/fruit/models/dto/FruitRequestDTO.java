ad0e28658fe4f62ffc10dc601edabbc6ab9dbb38
David Toledo <toledo_1990@hotmail.com>
2026-06-09 20:20:54 +0200

    feat: add fruit entity, repository and DTOs

--- src/main/java/cat/itacademy/s04/t02/n01/fruit/models/dto/FruitRequestDTO.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit/models/dto/FruitRequestDTO.java
@@ -1,4 +1,20 @@
 package cat.itacademy.s04.t02.n01.fruit.models.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class FruitRequestDTO {
+    @NotBlank
+    private String name;
+    @Positive
+    private int weightInKilos;
 }
--- src/main/java/cat/itacademy/s04/t02/n01/fruit/models/dto/FruitResponseDTO.java
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit/models/dto/FruitResponseDTO.java
@@ -1,4 +1,16 @@
 package cat.itacademy.s04.t02.n01.fruit.models.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class FruitResponseDTO {
+    private Long id;
+    private String name;
+    private int weightInKilos;
+
 }
