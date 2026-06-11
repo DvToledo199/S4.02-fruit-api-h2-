276b7628fcc608d71b39fb99d980b755e023eab7
David Toledo <toledo_1990@hotmail.com>
2026-06-11 23:02:33 +0200

    git commit -m "feat: implement fruit REST controller CRUD endpoints"

---
+++ src/main/java/cat/itacademy/s04/t02/n01/fruit/controllers/FruitController.java
@@ -0,0 +1,45 @@
+package cat.itacademy.s04.t02.n01.fruit.controllers;
+
+import cat.itacademy.s04.t02.n01.fruit.models.dto.FruitRequestDTO;
+import cat.itacademy.s04.t02.n01.fruit.services.FruitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import cat.itacademy.s04.t02.n01.fruit.models.dto.FruitResponseDTO;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fruits")
+@RequiredArgsConstructor
+public class FruitController {
+
+    private final FruitService fruitService;
+
+    @PostMapping
+    public FruitResponseDTO createFruit(
+            @RequestBody FruitRequestDTO requestDTO) {
+        return fruitService.createFruit(requestDTO);
+    }
+
+    @GetMapping
+    public List<FruitResponseDTO> getAllFruits() {
+        return fruitService.getAllFruits();
+    }
+
+    @GetMapping("/{id}")
+    public FruitResponseDTO getFruitById(@PathVariable Long id) {
+        return fruitService.getFruitById(id);
+    }
+
+    @PutMapping("/{id}")
+    public FruitResponseDTO updateFruit(@PathVariable Long id, @RequestBody FruitRequestDTO requestDTO) {
+        return fruitService.updateFruit(id, requestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFruit(@PathVariable Long id) {
+        fruitService.deleteFruit(id);
+    }
+
+
+}
